2133987962aae0998760909238519941a80e79ee
zuihou <244387066@qq.com>
2023-04-13 22:07:20 +0800

    doc: update [deploy]

--- docs/.vuepress/components/AdPage.vue
+++ docs/.vuepress/components/AdPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`wwads-cn wwads-${mode} page-wwads page-wwads-${content}`" data-id="231"></div>
+  <div :class="`wwads-cn wwads-${mode} page-wwads-${content}`" data-id="231"></div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -22,29 +22,8 @@ export default defineComponent({
 <style>
 @use "vuepress-shared/styles/wrapper";
 
-.page-wwads {
-  width:100%!important;
-  min-height: 0;
-  margin: 0;
-
-}
 .page-wwads-content {
   max-width: var(--content-width, 740px);
   margin: 0 auto;
-}
-.page-wwads .wwads-img img {
-  width:80px!important;
-}
-.page-wwads .wwads-poweredby {
-
-  width: 40px;
-  position: absolute;
-  right: 25px;
-  bottom: 3px;
-}
-.wwads-content .wwads-text, .page-wwads .wwads-text {
-  height: 100%;
-  padding-top: 5px;
-  display: block;
 }
 </style>
