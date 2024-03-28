b8d7a899bbe4bb3ad7a94371455c39e49a6f5152
zuihou <244387066@qq.com>
2024-03-28 18:36:15 +0800

    build: update [deploy]

--- docs/.vuepress/components/Sidebar.vue
+++ docs/.vuepress/components/Sidebar.vue
@@ -7,7 +7,7 @@
         </a>
         <div style="margin-top: 1.5rem; order: 9999;">
           <span style='color: gray;font-size: smaller;'></span>
-          <span style='color: #E01E5A;font-size: smaller;font-weight: bolder;float: right'>❤️<a href='/vip/广告位'>广告位招租</a></span>
+          <span style='color: #E01E5A;font-size: smaller;font-weight: bolder;float: right'>📢📢📢<a href='/vip/广告位.html'>免费加入此广告位</a></span>
         </div>
         <div style="order: 100000;background-color: rgba(0,0,0,.15);width:100%;height:1px;margin: 1rem 0 0 0; position:relative;">
           <button style='border-radius: 100%;padding: 0;text-align: center;border: none;background-color: #ff3861;cursor: pointer;position: absolute;left: calc(50% - 15px);top: -15px;height: 30px;width: 30px;color: #fff;' onclick='document.getElementById("sidebar-slot-top").style.display="none"'>收起</button>
